5fab92888792e684eaeebb27f19560bd907e6ee0
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-24 10:47:51 +0700

    rollback email field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name', 70);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('username')->unique();
             $table->string('password');
             $table->string('avatar')->nullable();
