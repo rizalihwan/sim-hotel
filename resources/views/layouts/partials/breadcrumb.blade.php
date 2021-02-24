4eb13b5a3e545e4afc764d46425b9039c422fd1e
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-24 21:41:58 +0700

    added breadcrumb partial

---
+++ resources/views/layouts/partials/breadcrumb.blade.php
@@ -0,0 +1,18 @@
+<div class="container-fluid">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-6">
+                <h3>Default</h3>
+            </div>
+            <div class="col-6">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a href="index.html"> <i data-feather="home"></i></a>
+                    </li>
+                    <li class="breadcrumb-item">Dashboard</li>
+                    <li class="breadcrumb-item active">Default </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
