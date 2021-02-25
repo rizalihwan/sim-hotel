3855d2d2a38ebc9cd389448e783e04f1e5525470
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-25 17:32:19 +0700

    fix breadcrumb

--- resources/views/layouts/partials/breadcrumb.blade.php
+++ resources/views/layouts/partials/breadcrumb.blade.php
@@ -2,15 +2,23 @@
     <div class="page-title">
         <div class="row">
             <div class="col-6">
-                <h3>Default</h3>
+                <h3 class="text-secondary"><u>Menu</u></h3>
             </div>
             <div class="col-6">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item">
-                        <a href="index.html"> <i data-feather="home"></i></a>
+                        <a href="{{ route('home') }}"> <i data-feather="home"></i></a>
                     </li>
-                    <li class="breadcrumb-item">Dashboard</li>
-                    <li class="breadcrumb-item active">Default </li>
+                    @if(request()->is('dashboard'))
+                        <li class="breadcrumb-item">Dashboard</li>
+                    @endif
+                    @if(request()->is('profile/setting'))
+                        <li class="breadcrumb-item">My Profile</li>
+                    @endif
+                    @if(request()->is('account/password'))
+                        <li class="breadcrumb-item">Account</li>
+                        <li class="breadcrumb-item">Change Password</li>
+                    @endif
                 </ol>
             </div>
         </div>
