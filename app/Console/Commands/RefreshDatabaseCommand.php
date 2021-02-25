6cdfa9fb90cbe3bd03e0e5727ee9b6c6c292949b
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-25 14:21:41 +0700

    create new command

---
+++ app/Console/Commands/RefreshDatabaseCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RefreshDatabaseCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:fresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This Command for re run all tables & run seeder scafollding.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('migrate:fresh');
+        $this->call('db:seed');
+        $this->info('Command run successfully!');
+    }
+}
