e97e532703e880110edd412a0072f3daa0c486b2
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-27 20:28:59 +0700

    modal for add new user

--- resources/views/admin/account/index.blade.php
+++ resources/views/admin/account/index.blade.php
@@ -45,28 +45,52 @@
               <h5 class="modal-title" id="exampleModalLabel2">Add New Account</h5>
               <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
             </div>
-            <div class="modal-body">
-              <form method="POST" enctype="multipart/form-data">
+            <form action="#" method="POST" enctype="multipart/form-data">
+              @csrf
+              <div class="modal-body">
                 <div class="row">
                   <div class="col-md-6">
                     <div class="form-group">
                       <label class="col-form-label" for="avatar">Image (Nullable):</label>
-                      <input class="form-control" type="file" name="avatar" id="avatar" placeholder>
+                      <input class="form-control" type="file" name="avatar" id="avatar" required>
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
                       <label class="col-form-label" for="name">Name:</label>
-                      <input class="form-control" type="text" name="name" id="name" placeholder>
+                      <input class="form-control" type="text" name="name" id="name" placeholder="your name" required>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label class="col-form-label" for="role">Role:</label>
+                      <select name="role" id="role" class="form-control custom-select" required>
+                        <option disabled selected>Select Role</option>
+                        <option value="admin">Admin</option>
+                        <option value="customer">Customer</option>
+                        <option value="boss">Boss</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label class="col-form-label" for="username">Username:</label>
+                      <input class="form-control" type="text" name="username" id="username" placeholder="username" required>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label class="col-form-label" for="password">Password:</label>
+                      <input class="form-control" type="password" name="password" id="password" placeholder="********" required>
                     </div>
                   </div>
                 </div>
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button class="btn btn-warning" type="button" data-dismiss="modal">Close</button>
-              <button class="btn btn-primary" type="button">Send message</button>
-            </div>
+              </div>
+              <div class="modal-footer">
+                <button class="btn btn-light" type="button" data-dismiss="modal">Close</button>
+                <button class="btn btn-primary" type="submit">Save</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
