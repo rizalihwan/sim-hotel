3fcf052f76091de7b14abadb22ea9073a117533a
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-27 21:18:57 +0700

    menu per role

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -15,20 +15,25 @@
                     <a href="#"><img class="img-fluid" src="{{ asset('assets/images/logo/logowebdark.png') }}" alt=""></a>
                     <div class="mobile-back text-right"><span>Back</span><i class="fa fa-angle-right pl-2" aria-hidden="true"></i></div>
                 </li>
-                <li class="sidebar-list"><a class="sidebar-link sidebar-title link-nav" href="{{ route('home') }}"><i data-feather="home"> </i><span>Dashboard</span></a></li>  
-                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="user"></i><span>Management Account</span></a>
-                    <ul class="sidebar-submenu">
-                        <li><a href="{{ route('admin.account.register.index') }}">Register Account</a></li>
-                        <li><a href="{{ route('admin.account.admin') }}">Admin Account</a></li>
-                        <li><a href="{{ route('admin.account.customer') }}">Customer Account</a></li>
-                        <li><a href="{{ route('admin.account.boss') }}">Boss Account</a></li>
-                    </ul>
-                </li>   
-                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="bar-chart"></i><span>Report</span></a>
-                    <ul class="sidebar-submenu">
-                        <li><a href="#">Apex Chart</a></li>
-                    </ul>
-                </li>
+                @role('boss')
+                    <li class="sidebar-list"><a class="sidebar-link sidebar-title link-nav" href="{{ route('home') }}"><i data-feather="home"> </i><span>Dashboard</span></a></li>  
+                @endrole
+                @role('admin')
+                    <li class="sidebar-list"><a class="sidebar-link sidebar-title link-nav" href="{{ route('home') }}"><i data-feather="home"> </i><span>Dashboard</span></a></li>  
+                    <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="user"></i><span>Management Account</span></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="{{ route('admin.account.register.index') }}">Register Account</a></li>
+                            <li><a href="{{ route('admin.account.admin') }}">Admin Account</a></li>
+                            <li><a href="{{ route('admin.account.customer') }}">Customer Account</a></li>
+                            <li><a href="{{ route('admin.account.boss') }}">Boss Account</a></li>
+                        </ul>
+                    </li>   
+                    <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="bar-chart"></i><span>Report</span></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="#">Apex Chart</a></li>
+                        </ul>
+                    </li>
+                @endrole
             </ul>
         </div>
         <div class="right-arrow" id="right-arrow"><i data-feather="arrow-right"></i></div>
