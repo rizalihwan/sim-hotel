a1f78269f9f6a78015aba72b41416ab2bfae94f5
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-27 14:03:42 +0700

    fix account & rolespermissions seeder

--- app/User.php
+++ app/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRoles;
 
     /**
      * The "booting" function of model
