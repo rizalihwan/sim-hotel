4a5cc680081e646acd618af66ebbd341f8df07ac
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-02 20:05:26 +0700

    added accessor for conditional rating, status etc

--- app/Room.php
+++ app/Room.php
@@ -13,8 +13,60 @@ class Room extends Model
     public $incrementing = false;
     protected $guarded = [];
 
+    protected $with = ['category'];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function getRoomThumbnailAttribute()
+    {
+        return "/storage/".$this->thumbnail;
+    }
+    
+    public function getRoomStatusAttribute()
+    {
+        return $this->status === 1 ? '<span class="badge badge-success">KOSONG<span>' : '<span class="badge badge-danger">DIISI<span>';
+    }
+    
+    public function getRatingCountAttribute()
+    {
+        $rating = $this->rating;
+        if($rating == 1)
+        {
+            return '<i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>';
+        } else if($rating == 2) {
+            return '<i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>';
+        } else if($rating == 3) {
+            return '<i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>';
+        } else if($rating == 4) {
+            return '<i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star-o font-primary"></i>';
+        } else if($rating == 5) {
+            return '<i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>
+                    <i class="fa fa-star font-primary"></i>';
+        } else {
+            return '<span class="badge badge-light">-<span>';
+        }
+    }
+
 }
