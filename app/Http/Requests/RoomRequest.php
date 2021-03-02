9e04aae7b8e157ac1695144aaeae00ecd2e4ba16
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-02 19:27:35 +0700

    added room request

---
+++ app/Http/Requests/RoomRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RoomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'room_code' => 'required|min:3',
+            'name' => 'required|max:50',
+            'thumbnail' => 'required|mimes:png,jpg,jpeg,svg|max:2048',
+            'floor' => 'required|max:3',
+            'category_id' => 'required',
+            'price' => 'required|integer',
+            'rating' => 'max:1'
+        ];
+    }
+}
