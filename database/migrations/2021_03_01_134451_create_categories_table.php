8f4a3cc33eacc2d99b19a24c10c87ef58d32f2a4
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-02 08:26:15 +0700

    update field type

--- database/migrations/2021_03_01_134451_create_categories_table.php
+++ database/migrations/2021_03_01_134451_create_categories_table.php
@@ -16,8 +16,8 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->text('description');
-            $table->string('facility');
+            $table->longText('description');
+            $table->text('facility');
             $table->timestamps();
         });
     }
