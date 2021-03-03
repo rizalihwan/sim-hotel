257d8bde6ec8ed6a23b6220e5a48fea6a19fe215
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-03 19:17:14 +0700

    add model & migration for booking

---
+++ database/migrations/2021_03_03_120646_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('booking_code')->unique();
+            $table->date('check_in'); 
+            $table->date('check_out');
+            $table->uuid('room_id');
+            $table->uuid('customer_id');
+            $table->string('payment_type'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
