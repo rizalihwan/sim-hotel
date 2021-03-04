8a4afe14f404fc09be9d0b49483a17cc51b348e9
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-04 22:20:21 +0700

    added mail vendor

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,24 +1,23 @@
-@extends('layouts.app')
+@extends('layouts.loglayout')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
+            <div class="card mt-5">
+                <div class="card-header">
+                    <u>Forgot Password</u>
+                </div>
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address:') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -29,17 +28,15 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        </div>   
+                    </div>
+                    <div class="card-footer">
+                        <a class="btn btn-danger" href="{{ route('login') }}">Back</a>
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Send Password Reset Link') }}
+                        </button>       
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.loglayout')
 
-@section('logincontent')
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
