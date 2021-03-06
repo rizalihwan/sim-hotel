ac347d1ea8b20f417d96dcc19444fa9fcea45110
muhamadhendrik <muhammadhendrik59@gmail.com>
2021-03-06 16:50:15 +0700

    show

--- resources/views/admin/category/detail.blade.php
+++ resources/views/admin/category/detail.blade.php
@@ -14,7 +14,8 @@
                         <label class="col-md-2 col-form-label mb-4">Description</label>
                         <div class="col-sm-1 d-none d-md-block"><span>:</span></div>
                         <div class="col-md-9">
-                            <textarea class="form-control mb-4" disabled>{{ $category->description }}</textarea>
+                            <textarea class="form-control mb-4" style="background-color:white;"
+                                disabled>{{ $category->description }}</textarea>
                         </div>
                         <label class="col-md-2 col-form-label mb-4">Facility</label>
                         <div class="col-sm-1 d-none d-md-block"><span>:</span></div>
