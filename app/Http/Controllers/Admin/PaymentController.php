a09fc05d687ed740bb659f7766bd59eeb5eb87c4
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-07 19:44:01 +0700

    Payment Controller index

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Booking;
+use App\Customer;
 use App\Http\Controllers\Controller;
+use App\Room;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
@@ -14,7 +17,12 @@ class PaymentController extends Controller
      */
     public function index()
     {
-        return view('admin.payment.index');
+        return view('admin.payment.index', [
+            'bookings' => Booking::orderBy('booking_code', 'ASC')->paginate(5),
+            'rooms' => Room::where('status', 1)->orderBy('room_code', 'ASC')->get(),
+            'check_room' => Room::orderBy('room_code', 'ASC')->get(),
+            'customers' => Customer::orderBy('first_name', 'ASC')->get()
+        ]);
     }
 
     /**
