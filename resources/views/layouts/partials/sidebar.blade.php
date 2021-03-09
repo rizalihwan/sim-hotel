b50247d203d894c7a49251fa0494b8340374db2e
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-09 19:59:55 +0700

    fix sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -55,10 +55,10 @@
                             <li><a href="{{ route('admin.report.finance') }}">Finance</a></li>
                         </ul>
                     </li>
-                    <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="align-center"></i>
+                    <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="align-center">
                             </i><span>Profile</span></a>
                         <ul class="sidebar-submenu">
-                            <li><a href="{{ route('profile.setting') }}">Account</a></li>
+                            <li><a href="{{ route('profile.setting') }}">My Profile</a></li>
                             <li><a data-toggle="modal" data-target="#exampleModalCenter" href="#">Log out</a></li>
                         </ul>
                     </li>
