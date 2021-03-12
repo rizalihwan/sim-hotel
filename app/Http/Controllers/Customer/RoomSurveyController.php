db74c368c0d982570e2d09ab4678bebcde861bc7
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-12 13:19:00 +0700

    fix roomsurveycontroller

--- app/Http/Controllers/Customer/RoomSurveyController.php
+++ app/Http/Controllers/Customer/RoomSurveyController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class RoomSurveyController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('customer.roomsurvey.index');
+    }
+
 }
