63c4347099a278cddaec5901cd806f8d0b37130a
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-12 13:10:18 +0700

    add room survey controller for customer

---
+++ app/Http/Controllers/Customer/RoomSurveyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RoomSurveyController extends Controller
+{
+    //
+}
