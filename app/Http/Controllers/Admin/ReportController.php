43ae726156fb761769afe5a3f55d71db0c929093
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-23 20:27:21 +0700

    fix report controller

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Booking;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 
 class ReportController extends Controller
 {
     public function finance()
     {
         return view('report.finance', [
+            'now' => Carbon::now(),
             'bookings' => Booking::where('status', 1)->orderBy('booking_code', 'ASC')->paginate(5)
         ]);
     }
