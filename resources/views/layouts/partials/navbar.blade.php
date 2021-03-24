af10096086a5753b9900eca0ded767d71b23d584
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-24 12:21:38 +0700

    fix navbar

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -54,27 +54,16 @@
                     <ul class="profile-dropdown onhover-show-div">
                         <li><a href="{{ route('profile.setting') }}" class="active"><i
                                     data-feather="user"></i><span>My Profile</span></a></li>
-                        <li><a data-toggle="modal" data-target="#exampleModalCenter"><i data-feather="log-in"> </i></i><span>Log Out</span></a></li>
+                        <li><a onclick="logout()"><i data-feather="log-in"> </i></i><span>Log Out</span></a></li>
                     </ul>
                 </li>
             </ul>
         </div>
-        <script class="result-template" type="text/x-handlebars-template">
-            <div class="ProfileCard u-cf">
-                <div class="ProfileCard-avatar"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay m-0"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path><polygon points="12 15 17 21 7 21 12 15"></polygon></svg></div>
-                <div class="ProfileCard-details">
-                    <div class="ProfileCard-realName">aksdnaskjd</div>
-                </div>
-            </div>
-        </script>
-        <script class="empty-template" type="text/x-handlebars-template">
-            <div class="EmptyMessage">Your search turned up 0 results. This most likely means the backend is down, yikes!</div>
-        </script>
     </div>
 </div>
 
 {{-- modal logout --}}
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenter"
+{{-- <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenter"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -92,7 +81,34 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 <form id="logout-form" action="{{ route('logout') }}" method="POST">
     @csrf
 </form>
+<script>
+    function logout()
+    {
+        Swal.fire({
+            title: 'Message Information!',
+            text: 'Are you sure you want to exit the application?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Logout!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Logout...",
+                        showConfirmButton: false,
+                        timer: 2300,
+                        timerProgressBar: true,
+                        onOpen: () => {
+                            document.getElementById('logout-form').submit();
+                            Swal.showLoading();
+                        }
+                    });
+                }
+        })
+    }
+</script>
