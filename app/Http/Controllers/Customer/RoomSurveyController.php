64eb2dcf975d49f72485084f7f1ea5b7cbb75023
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-25 16:32:33 +0700

    fix room survey controller

--- app/Http/Controllers/Customer/RoomSurveyController.php
+++ app/Http/Controllers/Customer/RoomSurveyController.php
@@ -14,4 +14,20 @@ class RoomSurveyController extends Controller
         ]);
     }
 
+    public function search()
+    {
+        $query = request('query');
+        $room = Room::with(['category'])
+                    ->where("name", "like", "%$query%")
+                    ->orWhere("room_code", "like", "%$query%")
+                    ->orWhere("price", "like", "%$query%")
+                    ->orWhereHas('category', function ($q) use ($query) {
+                        $q->where("name", "like", "%$query%");
+                    })
+                    ->orderBy('room_code', 'ASC')->paginate(4);
+        return view('customer.roomsurvey.index', [
+            'rooms' => $room
+        ]);
+    }
+
 }
