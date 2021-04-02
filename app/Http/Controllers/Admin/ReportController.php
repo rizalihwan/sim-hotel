251740f9abc825e14ec1583dd13b1458d673d47d
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-04-02 21:30:00 +0700

    tambah method print pdf

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Booking;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
+use PDF;
 
 class ReportController extends Controller
 {
@@ -15,4 +16,18 @@ class ReportController extends Controller
             'bookings' => Booking::where('status', 1)->orderBy('booking_code', 'ASC')->paginate(5)
         ]);
     }
+
+    public function finance_pdf()
+    {
+        return PDF::setOptions([
+            'dpi' => 150,
+            'defaultFont' => 'sans-serif',
+            'isHtml5ParserEnabled' => true, 
+            'isRemoteEnabled' => true
+        ])->loadView('report.finance_pdf', [
+            'now' => Carbon::now(),
+            'bookings' => Booking::where('status', 1)->orderBy('booking_code', 'ASC')->paginate(5)
+        ])->stream();
+    }   
+
 }
