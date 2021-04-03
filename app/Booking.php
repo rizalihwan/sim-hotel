6ffac07e6be474408dc3182173860d561e4efeab
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-04-03 10:49:00 +0700

    fix n+1 problem

--- app/Booking.php
+++ app/Booking.php
@@ -13,6 +13,8 @@ class Booking extends Model
     public $incrementing = false;
     protected $guarded = [];
 
+    protected $with = ['room', 'customer'];
+
     public function room()
     {
         return $this->belongsTo(Room::class);
