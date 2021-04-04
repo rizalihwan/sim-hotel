4f54587ffe6abe232c4f03241ffec9e22530da92
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-04-04 22:56:51 +0700

    tambah laporan booking

---
+++ resources/views/report/booking.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app', ['title' => 'HRI-HOTEL | BookingReport'])
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between">
+                    <div>
+                        @if(request('startDate') && request('endDate'))
+                            <a href="{{ route('admin.report.booking') }}" class="btn btn-danger btn-md"><i class="fa fa-arrow-left"></i></a>
+                        @endif
+                        @if(request('startDate') && request('endDate'))
+                            <a href="{{ route('admin.report.booking.pdf', ['startDate' => $startDate, 'endDate' => $endDate]) }}" class="btn btn-danger" target="_blank"><i class="fa fa-print"></i> Pdf</a>
+                        @else
+                            <a href="{{ route('admin.report.booking.pdf') }}" class="btn btn-danger" target="_blank"><i class="fa fa-print"></i> Pdf</a>
+                        @endif
+                    </div>
+                    <form action="{{ route('admin.report.booking.cari') }}" method="GET">
+                        @csrf
+                        <div class="d-flex justify-content-end mx-2">
+                            <label for="startDate" class="mr-2">Start Date</label>
+                            <input type="date" class="form-control @error('startDate') is-invalid @enderror" name="startDate" id="startDate" required>
+                            @error('startDate')
+                                <div class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @enderror
+                            <h3 class="mx-3">-</h3>
+                            <label for="endDate" class="mr-2">End Date</label>
+                            <input type="date" class="form-control @error('endDate') is-invalid @enderror mr-2" name="endDate" id="endDate" required>
+                            @error('endDate')
+                                <div class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @enderror
+                            <button type="submit" class="btn btn-secondary btn-sm"><i class="fa fa-search"></i> Cari</button>      
+                        </div> 
+                    </form> 
+                </div>
+                @if(request('startDate') && request('endDate'))
+                    <span class="badge badge-light mx-5 mt-3"><h6 class="p-1">Laporan From : {{ $startDate }} &middot; To : {{ $endDate }}</h6></span></th>
+                @endif
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Booking Code</th>
+                                    <th>Payment Status</th>
+                                    <th>Checkin</th>
+                                    <th>Checkout</th>
+                                    <th>Room</th>
+                                    <th>Customer</th>
+                                    <th>Payment Type</th>
+                                </tr>
+                            </thead>
+                            @forelse ($bookings as $booking)
+                                <tbody>
+                                    <th>{{ $loop->iteration + $bookings->firstItem() - 1 . '.' }}</th>
+                                    <td><u>{{ $booking->booking_code }}</u></td>
+                                    <td>{!! $booking->PaymentStatus !!}</td>
+                                    <td>{{ $booking->check_in }}</td>
+                                    <td>{{ $booking->check_out }}</td>
+                                    <td>
+                                        <span class="badge badge-light">{{ Str::upper($booking->room->name . " (" . $booking->room->category->name . ")") }}<span>
+                                    </td>
+                                    <td>
+                                        <span class="badge badge-light">{{ Str::upper($booking->customer->FullName) }}<span>
+                                    </td>
+                                    <td>
+                                        <span class="badge badge-light">{{ Str::upper($booking->payment_type) }}<span>
+                                    </td>
+                                </tbody>
+                            @empty
+                                <tbody>
+                                    <tr>
+                                        <th colspan="8" style="color: red; text-align: center;">Data Empty!</th>
+                                    </tr>
+                                </tbody>
+                            @endforelse
+                        </table>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    {{ $bookings->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/report/booking_pdf.blade.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <title>Booking_Report</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                @if(request('startDate') && request('endDate'))
+                    <h6 class="text-secondary"><u>Laporan From : {{ $startDate }} &middot; To : {{ $endDate }}</u></h6>
+                @endif
+            </div>
+        </div>
+        <hr class="mb-3">
+        <div class="row">
+            <div class="col-md-12">
+                <div style="text-align: center;">
+                    <div>
+                        <strong>Laporan Booking</strong>
+                    </div>
+                    <small style="margin : 0px; display: block;">Alamat : Kp tenggek, Desa Cimande hilir Kecamatan caringin, Kabupaten bogor, kode POS : 16730</small>
+                    <small style="margin : 0px; display: block;">Linkedln : rizalihwan</small>
+                    <small style="margin : 0px; display: block;">Email : rizalihwan94@gmail.com</small>
+                    <p style="margin : 0px;">HP/WA : 085770254568</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row my-3">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table border="1" width="100%" cellspacing="0" cellpadding="10" class="table table-striped">
+                        <tr>
+                            <th>#</th>
+                            <th>Booking Code</th>
+                            <th>Payment Status</th>
+                            <th>Checkin</th>
+                            <th>Checkout</th>    
+                        </tr>
+                        @forelse ($bookings as $booking)
+                        <tr>
+                            <th>{{ $loop->iteration . '.' }}</th>
+                            <td><u>{{ $booking->booking_code }}</u></td>
+                            <td>{!! $booking->PaymentStatus !!}</td>
+                            <td>{{ $booking->check_in }}</td>
+                            <td>{{ $booking->check_out }}</td>
+                        </tr>
+                        @empty
+                            <tr>
+                                <th colspan="8" style="color: red; text-align: center;">Data Empty!</th>
+                            </tr>    
+                        @endforelse
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
