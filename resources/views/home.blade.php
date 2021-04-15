e5b361ef34f4f3f249585dd8d06803d81a1b43ff
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-04-15 22:54:55 +0700

    hapus yang tidak diperlukan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,7 +36,29 @@
                                             </div>
                                         </div>
                                         <div class="row">
-                                          <div id="testChart" style="width: 100%;"></div>
+                                          <div class="col-md-6">
+                                            <div class="card">
+                                              <div class="card-header">
+                                                  <h6 class="mb-2"><u>Room Status</u></h6>
+                                                  <div>
+                                                      <span class="badge badge-pill badge-success">&middot;</span>
+                                                      <small class="text-secondary mr-2">Empty Room</small>
+                                                      <span class="badge badge-pill badge-danger">&middot;</span>
+                                                      <small class="text-secondary">Room Filled</small>
+                                                  </div>
+                                              </div>
+                                              <div class="card-body">
+                                                  @foreach ($check_room as $room)
+                                                      @if ($room->status === 0)
+                                                          <span style="margin: 0;" class="badge badge-danger mb-1">{{ $room->room_code }}</span>
+                                                      @endif
+                                                      @if ($room->status === 1)
+                                                          <span style="margin: 0;" class="badge badge-success mb-1">{{ $room->room_code }}</span>
+                                                      @endif
+                                                  @endforeach
+                                              </div>
+                                            </div>  
+                                          </div>
                                         </div>
                                     </div>
                                 </div>
@@ -90,74 +112,4 @@
         </div>
     @endrole
 </div>
-@endsection
-@section('script')
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script>
-    Highcharts.chart('testChart', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: 'Monthly Average Rainfall'
-    },
-    subtitle: {
-        text: 'Source: WorldClimate.com'
-    },
-    xAxis: {
-        categories: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-            'Oct',
-            'Nov',
-            'Dec'
-        ],
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Rainfall (mm)'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        name: 'Tokyo',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-    }, {
-        name: 'New York',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-    }, {
-        name: 'London',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-    }, {
-        name: 'Berlin',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-    }]
-});
-</script>
 @endsection
