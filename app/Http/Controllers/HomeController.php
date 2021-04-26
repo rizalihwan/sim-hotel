5caecd734a6d50d7734a4f8ccffdccac0a20e337
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-04-26 22:31:43 +0700

    perbaiki home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
             'bookings' => Booking::count(),
             'customers' => Customer::count(),
             'categories' => Category::orderBy('name', 'ASC')->get(),
-            'finances' => Booking::where('status', 1)->orderBy('booking_code', 'ASC')->paginate(10)
+            'finances' => Booking::where('status', 1)->latest()->take(1)->get()
         ]);
     }
 }
