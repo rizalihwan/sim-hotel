f7d0bf41a8f5eaee1ba9ea00dc3ca631fb50ffe5
muhamadhendrik <muhammadhendrik59@gmail.com>
2021-05-08 12:53:23 +0700

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,11 +65,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user =  User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'username' => $data['username'],
             'password' => Hash::make($data['password']),
         ]);
+        $user->assignRole('customer');
+        return $user;
+
     }
 }
