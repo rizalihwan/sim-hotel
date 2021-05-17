236d02da1cbbca46a8f962628008e3cf5f0f39f7
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-05-17 19:31:28 +0700

    fix register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,16 +5,14 @@
             <div class="col-12">
                 <div class="login-card">
                     <div>
-                        <div class="logo"><img class="img-fluid for-light"
-                                src="{{ asset('assets/images/logo/logoweb.png') }}" alt="looginpage"
-                                style="height: 100px; width: 300px; object-fit: cover;"><img class="img-fluid for-dark"
-                                src="{{ asset('assets/images/logo/logowebdark.png') }}" alt="looginpage"
-                                style="height: 100px; width: 300px; object-fit: cover;"></div>
                         <div class="login-main">
                             <form class="theme-form" method="POST" action="{{ route('register') }}">
                                 @csrf
-                                <h4>Create your account</h4>
-                                <p>Enter your personal details to create account</p>
+                                <div class="logo"><img class="img-fluid for-light"
+                                        src="{{ asset('assets/images/logo/logoweb.png') }}" alt="looginpage"
+                                        style="height: 100px; width: 300px; object-fit: cover;"><img
+                                        class="img-fluid for-dark" src="{{ asset('assets/images/logo/logowebdark.png') }}"
+                                        alt="looginpage" style="height: 100px; width: 300px; object-fit: cover;"></div>
                                 <div class="form-group">
                                     <label class="col-form-label">Your Name</label>
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
@@ -30,7 +28,7 @@
                                 <div class="form-group">
                                     <label class="col-form-label">Username</label>
                                     <input id="username" class="form-control @error('username') is-invalid
-                                    @enderror" name="username" value="{{ old('username') }}" type="text" required
+                                        @enderror" name="username" value="{{ old('username') }}" type="text" required
                                         placeholder="username" autofocus>
 
                                     @error('username')
@@ -62,13 +60,13 @@
                                         </span>
                                     @enderror
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group mb-2">
                                     <label class="col-form-label">Confirm Password</label>
                                     <input id="password-confirm" type="password" class="form-control"
                                         name="password_confirmation" placeholder="type again your password" required
                                         autocomplete="new-password">
                                 </div>
-                                <div class="form-group mb-0">
+                                <div class="form-group mt-1">
                                     <button class="btn btn-primary btn-block" type="submit">Create Account</button>
                                 </div>
                                 <p class="mt-4 mb-0">Already have an account?<a class="ml-2"
