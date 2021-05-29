2d1fefb59bb212afa1aa4fc965279324112baf67
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-05-29 19:55:06 +0700

    new column for on feedback customer

--- database/migrations/2021_05_29_194948_add_user_id_to_bookings_table.php
+++ database/migrations/2021_05_29_194948_add_user_id_to_bookings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddEmailToBookingsTable extends Migration
+class AddUserIdToBookingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddEmailToBookingsTable extends Migration
     public function up()
     {
         Schema::table('bookings', function (Blueprint $table) {
-            $table->string('email')->nullable()->after('thumbnail');
+            $table->foreignId('user_id')->nullable()->default(null)->after('thumbnail');
         });
     }
 
@@ -26,7 +26,7 @@ class AddEmailToBookingsTable extends Migration
     public function down()
     {
         Schema::table('bookings', function (Blueprint $table) {
-            $table->dropColumn('email');
+            $table->dropColumn('user_id');
         });
     }
 }
