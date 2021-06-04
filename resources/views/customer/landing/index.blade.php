1fb146ef2e68f44496e8bbf04e7e3637c4e603bf
muhamadhendrik <muhammadhendrik59@gmail.com>
2021-06-04 15:07:41 +0700

    landing fix

--- resources/views/customer/landing/index.blade.php
+++ resources/views/customer/landing/index.blade.php
@@ -38,83 +38,15 @@
                     </div>
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-xs-6">
-                                <select class="form-control">
-                                    <option>No. of Rooms</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </select>
-                            </div>
-                            <div class="col-xs-6">
-                                <select class="form-control">
-                                    <option>No. of Adult</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-xs-4">
-                                <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
-                                    <option>Date</option>
-                                    <option value="01">1</option>
-                                    <option value="02">2</option>
-                                    <option value="03">Mar (03)</option>
-                                    <option value="04">Apr (04)</option>
-                                    <option value="05">May (05)</option>
-                                    <option value="06">June (06)</option>
-                                    <option value="07">July (07)</option>
-                                    <option value="08">Aug (08)</option>
-                                    <option value="09">Sep (09)</option>
-                                    <option value="10">Oct (10)</option>
-                                    <option value="11">Nov (11)</option>
-                                    <option value="12">Dec (12)</option>
-                                </select>
-                            </div>
-                            <div class="col-xs-4">
-                                <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
-                                    <option>Month</option>
-                                    <option value="01">Jan (01)</option>
-                                    <option value="02">Feb (02)</option>
-                                    <option value="03">Mar (03)</option>
-                                    <option value="04">Apr (04)</option>
-                                    <option value="05">May (05)</option>
-                                    <option value="06">June (06)</option>
-                                    <option value="07">July (07)</option>
-                                    <option value="08">Aug (08)</option>
-                                    <option value="09">Sep (09)</option>
-                                    <option value="10">Oct (10)</option>
-                                    <option value="11">Nov (11)</option>
-                                    <option value="12">Dec (12)</option>
-                                </select>
-                            </div>
-                            <div class="col-xs-4">
-                                <select class="form-control" name="expiry-year">
-                                    <option value="13">2013</option>
-                                    <option value="14">2014</option>
-                                    <option value="15">2015</option>
-                                    <option value="16">2016</option>
-                                    <option value="17">2017</option>
-                                    <option value="18">2018</option>
-                                    <option value="19">2019</option>
-                                    <option value="20">2020</option>
-                                    <option value="21">2021</option>
-                                    <option value="22">2022</option>
-                                    <option value="23">2023</option>
-                                </select>
+                            <div class="col-xs-12">
+                                <input type="text" placeholder="No. of Rooms" class="form-control">
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
                         <textarea class="form-control" placeholder="Message" rows="4"></textarea>
                     </div>
-                    <button class="btn btn-default">Submit</button>
+                    <a href="{{ route('login') }}" class="btn btn-default">Submit</a>
                 </form>
             </div>
         </div>
